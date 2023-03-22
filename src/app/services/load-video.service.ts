94323062e60055a973089231da892be9b3dac453
Rahul Sharma <rahu8299@gmail.com>
2023-03-22 23:57:51 +0530

    in progress file input component

--- src/app/services/load-video.service.ts
+++ src/app/services/load-video.service.ts
@@ -1,36 +1,39 @@
-import { Injectable, ViewChild } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoadVideoService {
-  VideoDownloadProgress!: number;
-  videoBlob!: Blob;
-  video: any;
+  videoBlobUrl: any;
+  video : any
+  
+  // VideoDownloadProgress!: number;
+  // videoBlob!: Blob;
+  // video: any;
 
-  constructor() {}
+  // constructor() {}
 
-  async downloadVideo(url: string) {
-    let response = await fetch(url);
-    const reader = response.body!.getReader();
-    const contentLength = response.headers.get('Content-Length') || '0';
-    let receivedLength = 0; // received that many bytes at the moment
-    let chunks = []; // array of received binary chunks (comprises the body)
-    while (true) {
-      const { done, value } = await reader.read();
-      if (done) break;
+  // async downloadVideo(url: string) {
+  //   let response = await fetch(url);
+  //   const reader = response.body!.getReader();
+  //   const contentLength = response.headers.get('Content-Length') || '0';
+  //   let receivedLength = 0; // received that many bytes at the moment
+  //   let chunks = []; // array of received binary chunks (comprises the body)
+  //   while (true) {
+  //     const { done, value } = await reader.read();
+  //     if (done) break;
 
-      chunks.push(value);
-      receivedLength += value.length;
+  //     chunks.push(value);
+  //     receivedLength += value.length;
 
-      this.VideoDownloadProgress =
-        (receivedLength / parseInt(contentLength)) * 100;
-      // console.log(
-      //   `Received ${receivedLength} of ${contentLength}`,
-      //   (receivedLength / parseInt(contentLength)) * 100
-      // );
-    }
-    this.videoBlob = new Blob(chunks);
-    return this.videoBlob;
-  }
+  //     this.VideoDownloadProgress =
+  //       (receivedLength / parseInt(contentLength)) * 100;
+  //     // console.log(
+  //     //   `Received ${receivedLength} of ${contentLength}`,
+  //     //   (receivedLength / parseInt(contentLength)) * 100
+  //     // );
+  //   }
+  //   this.videoBlob = new Blob(chunks);
+  //   return this.videoBlob;
+  // }
 }
