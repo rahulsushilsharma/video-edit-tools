08bdbf3f8525abecef65d78645dee34ee246de13
Rahul Sharma <rahu8299@gmail.com>
2023-05-14 19:59:15 +0530

    updates in sidebar and sidebar icons

--- src/app/services/ui-controls.service.ts
+++ src/app/services/ui-controls.service.ts
@@ -5,10 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class UiControlsService {
 
+  
+
   TrimComponentViewToggle: boolean = false
   ProgressComponentViewToggle : boolean = false
   FileInputComponentViewToggle : boolean = false
   FileSystemComponentViewToggle :boolean = false
   FormatConverterComponentViewToggle :boolean = false
   constructor() { }
+
+  
+  
 }
